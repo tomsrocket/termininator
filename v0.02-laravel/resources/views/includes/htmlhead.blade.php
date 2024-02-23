35062ee7eb27ec665c9769de1a2a60423a677469
Thomas Werner <thomas.w.werner@gmail.com>
2024-02-23 19:01:07 +0100

    Add bootstrap icons and favicon

--- v0.02-laravel/resources/views/includes/htmlhead.blade.php
+++ v0.02-laravel/resources/views/includes/htmlhead.blade.php
@@ -6,6 +6,8 @@
 
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="icon" type="image/x-icon" href="/favicon.ico">
+
 
 <title>{{ config('app.name', 'Laravel') }}</title>
 
