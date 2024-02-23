2f6eef6dbcefa7c055f3d0521cbff2bb58197563
Thomas Werner <thomas.w.werner@gmail.com>
2024-02-23 18:49:28 +0100

    Add Font and navbar logo

--- v0.02-laravel/resources/views/includes/header.blade.php
+++ v0.02-laravel/resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Fixed navbar</a>
+      <a class="navbar-brand tfont" href="#">TERMIN<span class="tfont2">INATOR</span></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -11,7 +11,7 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="{{ url('/events') }}">Terminplanung</a>
           </li>
           <li class="nav-item">
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
--- v0.02-laravel/vite.config.js
+++ v0.02-laravel/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -11,4 +12,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+          '~font' : path.resolve(__dirname,'resources/assets/fonts')
+        }
+    }
 });
